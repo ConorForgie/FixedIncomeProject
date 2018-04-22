5fc39a6f86cf43b3de7c627ec14bc325a7e34bcb
Conor Forgie <conor.forgie@gmail.com>
2018-04-22 20:47:53 +0100

    Demo system call of python script from program

--- FixedIncomeProject/Main.cpp
+++ FixedIncomeProject/Main.cpp
@@ -1,10 +1,22 @@
 // FixedIncomeProject.cpp : Defines the entry point for the console application.
 //
 #include <iostream>
+#include <stdlib.h>
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    /*
+    // Demo python script call NOTE for WINDOWS \\ or for LINUX /
+    // WINDOWS
+    std::string filename = "C:\\Users\\con-4\\Documents\\Uni\\CMF\\Thesis\\DeltaHedgingCall.py";
+    std::string command = "python ";
+    // LINUX
+    // std::string filename = "/mnt/c/Users/con-4/Documents/Uni/CMF/Thesis/DeltaHedgingCall.py";
+    // std::string command = "python3 ";
+    command += filename;
+    system(command.c_str());
+    */
+
     return 0;
 }
 

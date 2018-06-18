455c69f974bceee94822b2d251232865d1d113f3
Conor Forgie <s1326353@sms.ed.ac.uk>
2018-06-18 10:42:06 +0100

    New Bond and RootFinder classes

--- FixedIncomeProject/Main.cpp
+++ FixedIncomeProject/Main.cpp
@@ -2,6 +2,9 @@
 //
 #include <iostream>
 #include <stdlib.h>
+#include <cmath>
+#include "RootFinder.h"
+#include "Bond.h"
 
 int main()
 {
@@ -17,6 +20,32 @@ int main()
     system(command.c_str());
     */
 
+	double yield = 0.01;
+	double coupon_rate = 0.05;
+	double face_value = 100;
+	double tenor = 10;
+
+	Bond b(yield, coupon_rate, face_value, tenor);
+
+	std::cout << "Bond NPV: " << b.NetPresentValue() << " DV01: " << b.DV01() << std::endl;	
+
+	auto nBNPV = [&b](double y) {return b.PriceFromYield(y) ; };
+	RootFinder finder(1e-6,1e4);
+	try
+	{
+		double root = finder.SecantMethod(nBNPV, 1E-5, 0.2);
+		std::cout << "Yeild from price " << root << std::endl << "Number of iterations " << finder.GetNumberOfIterations() << std::endl;
+	}
+	catch (std::exception e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+
+
+	
+	
+	int a;
+	std::cin >> a;
     return 0;
 }
 
